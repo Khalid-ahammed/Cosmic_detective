8ad22833538e4d3be9fb1ecd8ead35091fab0866
Khalid-ahammed <71166677+Khalid-ahammed@users.noreply.github.com>
2022-03-12 12:54:11 +0600

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -93,6 +93,7 @@ const Navbar = () => {
                         ? ' menu-active'
                         : ''
                     }`}
+                    onClick={handleNavClose}
                   >
                     {menuItem.title}
                   </Link>
